b8b502ef0f8cac17f4a17790ca47e6af5bdacfc3
Peter <peter_shuai@yeah.net>
2017-04-17 12:54:41 +0800

    提交两个测试并试一下怎么跟eclipse的工作空间链接

---
+++ Algorithms_Code/src/test_1.java
@@ -0,0 +1,6 @@
+
+public class test_1 {
+	public static void main(String args[]){
+		System.out.println("Hello World !");
+	}
+}
---
+++ Algorithms_Code/src/test_2.java
@@ -0,0 +1,8 @@
+
+public class test_2 {
+	public static void main(String args[]){
+		int i = 0 ;
+		i = 1 + 1 ;
+		System.out.println(i);
+	}
+}
